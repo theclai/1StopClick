7400c37a9ddc11173893faa52b45377cbeed3ac1
Faisal Rahman <faisal@plank.life>
2019-04-24 08:06:45 +0800

    add enum

---
+++ src/main/java/com/mitrais/cdc/domain/enumeration/PaymentMethod.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 20042019 All Rights Reserved.
+ */
+package com.mitrais.cdc.domain.enumeration;
+
+/**
+ * @author Faisal_RM754
+ * @version $Id: PaymentMethod.java, v 0.1 20190424 8:06 Faisal_RM754 Exp $$
+ */
+public enum PaymentMethod {
+    CREDIT_CARD, CASH_ON_DELIVERY, PAYPAL
+}
